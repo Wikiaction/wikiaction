38c992299be138caba96ad145e2cba29c6e87ce4
Gael Kijko <gaelkijko@hotmail.com>
2018-05-27 18:18:13 -0400

    test_gk

--- app/about.php
+++ app/about.php
@@ -20,7 +20,7 @@
 		<div class="about__text">
 			<p><b>Wikiaction est une plateforme expérimentale d’engagement collectif. Au lieu de vous demander d’agir tout de suite, nous vous proposons de conditionner votre engagement à l’efficacité d’un projet collectif. Les engagements prennent effet lorsque suffisamment de personnes sont mobilisées.</b></p>
 			<br>
-			<h3>MISSION</h3>
+			<h3>MISSION:</h3>
 			<p>Notre plateforme permet de mener à bien des projets collectifs complexe incluant notamment:
 			<ul>
 				<li>L'organisation d’activités bénévoles</li>
@@ -29,16 +29,16 @@
 			</ul>
 			Wikiaction est une organisation à but non lucratif conçue de façon entièrement bénévole. Nous garantissons l’anonymat des membres et ne participons d’aucune manière au commerce de vos données. La plateforme se veut aussi ouverte à la participation de la communauté grâce à forum de débat qui prend en compte les arguments les plus votés.</p>
 			<br>
-			<h3>CONCEPTION</h3>
+			<h3>CONCEPTION:</h3>
 			<p>Notre plateforme permet la création de ces projets collectifs et la coordination de plusieurs parties prenantes. Les engagés ne sont pas tenus de passer à l’acte avant le franchissement d’un nombre minimum de personnes nécessaires à l’efficacité du projet.</p>
 			<br>
-			<h3>CRÉATION</h3>
+			<h3>CRÉATION:</h3>
 			<p>Wikiaction permet la création de projets très variés. Par exemple, les parties prenantes peuvent être indépendantes les unes des autres, ou non. Leurs engagements peuvent être ponctuels, récurrents (pendant une période limitée), ou permanents (ce qui correspondrait à une nouvelle habitude à prendre). On permet en outre la possibilité d’agir avant le franchissement du seuil, ou de dépasser le seuil - tout cela à la discrétion du créateur.</p>
 			<br>
-			<h3>ÉVOLUTION</h3>
+			<h3>ÉVOLUTION:</h3>
 			<p>Les projets collectifs correspondent rarement à la manière dont ils avaient été imaginés au départ. L’évolution du contexte, et surtout le partage des connaissances des parties prenantes tend à faire évoluer le projet. Afin de rendre cette évolution possible, nous avons permis au créateur de modifier les paramètres de son projet.</p>
 			<br>
-			<h3>LICENSE</h3>
+			<h3>LICENSE:</h3>
 			<p>
 				<a href="https://wikiaction.org/">Fondé à Montréal, Wikiaction</a> est mis à disposition selon les termes de la <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International</a>.
 				<br>
